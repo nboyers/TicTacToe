33a8b7ca413708b50837ae17c71f6034fd57ed62
nboyers <frenttv@gmail.com>
2022-01-05 14:09:13 -0500

    cleanup unused code

--- TicTacToe/GameModel/Board.swift
+++ TicTacToe/GameModel/Board.swift
@@ -41,10 +41,6 @@ struct Board {
         self.lastMove = lastMove
     }
     
-    var winPattern: Set<Set<Int>> = [ [0,1,2],[3,4,5],
-                                      [6,7,8],[0,3,6],
-                                      [1,4,7],[2,5,8],
-                                      [0,4,8],[2,4,6] ]
     var legalMoves: [Move] {
         return position.indices.filter { position[$0] == .E }
     }
